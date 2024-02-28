b7644c21c967e605326e63af54bf53cabdb2be7f
huynv.tvf <huynv1@techvify.com.vn>
2024-02-28 15:34:35 +0700

    Added more module

---
+++ candlizer/src/main/java/com/tbot/cyclop/candlizer/CandlizerApplication.java
@@ -0,0 +1,13 @@
+package com.tbot.cyclop.candlizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CandlizerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CandlizerApplication.class, args);
+    }
+
+}
---
+++ candlizer/src/test/java/com/tbot/cyclop/candlizer/CandlizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tbot.cyclop.candlizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CandlizerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
