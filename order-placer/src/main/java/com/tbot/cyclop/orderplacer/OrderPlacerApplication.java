94b232789a80f393528461f0a8553470010f6611
huynv.tvf <huynv1@techvify.com.vn>
2024-02-28 15:31:58 +0700

    Added more module

---
+++ order-placer/src/main/java/com/tbot/cyclop/orderplacer/OrderPlacerApplication.java
@@ -0,0 +1,13 @@
+package com.tbot.cyclop.orderplacer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderPlacerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderPlacerApplication.class, args);
+    }
+
+}
