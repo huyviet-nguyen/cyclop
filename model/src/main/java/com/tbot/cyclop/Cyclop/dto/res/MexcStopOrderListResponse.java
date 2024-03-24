ff7be4a5388c95fce5b29ff1d61105ec73a3bd4d
Huey <huyviet.java@gmail.com>
2024-03-25 02:03:42 +0700

    reduce unused import

--- model/src/main/java/com/tbot/cyclop/Cyclop/dto/res/MexcStopOrderListResponse.java
+++ model/src/main/java/com/tbot/cyclop/Cyclop/dto/res/MexcStopOrderListResponse.java
@@ -1,7 +1,6 @@
 package com.tbot.cyclop.Cyclop.dto.res;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 import java.util.List;
