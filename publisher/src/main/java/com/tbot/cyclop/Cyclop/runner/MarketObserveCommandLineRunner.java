d8bec51988afa9162000c4cd033cd8c5b9798d8e
Huey <huyviet.java@gmail.com>
2024-03-27 21:19:44 +0700

    reduce compress

--- publisher/src/main/java/com/tbot/cyclop/Cyclop/runner/MarketObserveCommandLineRunner.java
+++ publisher/src/main/java/com/tbot/cyclop/Cyclop/runner/MarketObserveCommandLineRunner.java
@@ -77,7 +77,7 @@ public class MarketObserveCommandLineRunner implements CommandLineRunner {
                 concurrentHashMap.put(klineData.getKafkaKey(), 0);
                 return null;
             }
-            if (concurrentHashMap.get(klineData.getKafkaKey()) == 8) {
+            if (concurrentHashMap.get(klineData.getKafkaKey()) == 4) {
                 concurrentHashMap.put(klineData.getKafkaKey(), 0);
                 return klineData;
             } else {
